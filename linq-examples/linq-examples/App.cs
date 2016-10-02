375b9c6660e1d77f3dfd1902e2770c65b65ea11c
matheusmorcinek <matheusmorcinek@gmail.com>
2016-10-01 21:45:38 -0300

    Base - App (Added)

---
+++ linq-examples/linq-examples/App.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq_examples
+{
+    class App
+    {
+
+
+
+
+        static void Main(string[] args)
+        {
+
+
+            string[] lista = new string[] { "Matheus", "Moreira", "Morcinek" };
+
+            var stringQuery = from s in lista
+                              where s.Equals("Morcinek")
+                              select s;
+
+            foreach(string str in stringQuery)
+            {
+                Console.WriteLine("Resulatdo -> : " + str);
+            } 
+
+        }
+
+
+    }
+}
